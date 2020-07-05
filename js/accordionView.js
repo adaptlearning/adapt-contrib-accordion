d6695f691ede55ff7ef602da400c75c59f17d3de
Oliver Foster <olivermartinfoster@gmail.com>
2020-07-06 00:01:04 +0100

    issue/2824-jsx React version of accordion

--- js/accordionView.js
+++ js/accordionView.js
@@ -1,84 +1,58 @@
-define([
-  'core/js/views/componentView'
-], function(ComponentView) {
+import ComponentView from 'core/js/views/componentView';
 
-  var AccordionView = ComponentView.extend({
+class AccordionView extends ComponentView {
 
-    events: {
-      'click .js-toggle-item': 'onClick'
-    },
+  preRender() {
+    this.checkIfResetOnRevisit();
+    this.model.resetActiveItems();
+    this.listenTo(this.model.get('_children'), {
+      'change:_isActive': this.onItemsActiveChange,
+      'change:_isVisited': this.onItemsVisitedChange,
+      'all': this.changed
+    });
+  }
 
-    preRender: function() {
-      this.checkIfResetOnRevisit();
-
-      this.model.resetActiveItems();
-
-      this.listenTo(this.model.get('_children'), {
-        'change:_isActive': this.onItemsActiveChange,
-        'change:_isVisited': this.onItemsVisitedChange
-      });
-    },
-
-    postRender: function() {
-      this.setReadyStatus();
-
-      if (this.model.get('_setCompletionOn') === 'inview') {
-        this.setupInviewCompletion();
-      }
-    },
-
-    checkIfResetOnRevisit: function() {
-      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
-
-      // If reset is enabled set defaults
-      if (isResetOnRevisit) {
-        this.model.reset(isResetOnRevisit);
-      }
-    },
-
-    onClick: function(event) {
-      event.preventDefault();
-
-      this.model.toggleItemsState($(event.currentTarget).parent().data('index'));
-    },
-
-    onItemsActiveChange: function(item, isActive) {
-      this.toggleItem(item, isActive);
-    },
-
-    onItemsVisitedChange: function(item, isVisited) {
-      if (!isVisited) return;
-
-      var $item = this.getItemElement(item);
-
-      $item.children('.accordion__item-btn').addClass('is-visited');
-    },
-
-    toggleItem: function(item, shouldExpand) {
-      var $item = this.getItemElement(item);
-      var $body = $item.children('.accordion__item-content').stop(true, true);
-
-      $item.children('.accordion__item-btn')
-        .toggleClass('is-selected is-open', shouldExpand)
-        .toggleClass('is-closed', !shouldExpand)
-        .attr('aria-expanded', shouldExpand);
-
-      if (!shouldExpand) {
-        $body.slideUp(this.model.get('_toggleSpeed'));
-        return;
-      }
-
-      $body.slideDown(this.model.get('_toggleSpeed'));
-    },
-
-    getItemElement: function(item) {
-      var index = item.get('_index');
-
-      return this.$('.accordion__item').filter('[data-index="' + index +'"]');
+  postRender() {
+    this.setReadyStatus();
+    if (this.model.get('_setCompletionOn') === 'inview') {
+      this.setupInviewCompletion();
     }
+  }
 
-  });
+  checkIfResetOnRevisit() {
+    var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+    // If reset is enabled set defaults
+    if (isResetOnRevisit) {
+      this.model.reset(isResetOnRevisit);
+    }
+  }
 
-  return AccordionView;
+  onClick(event) {
+    event.preventDefault();
+    this.model.toggleItemsState($(event.currentTarget).parent().data('index'));
+  }
 
-});
+  onItemsActiveChange(item, isActive) {
+    this.toggleItem(item, isActive);
+  }
+
+  toggleItem(item, shouldExpand) {
+    var $item = this.getItemElement(item);
+    var $body = $item.children('.accordion__item-content').stop(true, true);
+    if (!shouldExpand) {
+      $body.slideUp(this.model.get('_toggleSpeed'));
+      return;
+    }
+    $body.slideDown(this.model.get('_toggleSpeed'));
+  }
+
+  getItemElement(item) {
+    var index = item.get('_index');
+    return this.$('.accordion__item').filter('[data-index="' + index +'"]');
+  }
+
+}
+
+AccordionView.template = 'accordion.jsx';
+
+export default AccordionView;
---
+++ templates/accordion.jsx
@@ -0,0 +1,60 @@
+import { compile, classes, templates, html } from 'core/js/reactHelpers';
+
+export default function (view, data) {
+  return <div className="component__inner accordion__inner">
+    {templates.component(view, data)}
+    <div className="component__widget accordion__widget">
+      {data._items.map(({ _id, _graphic, _classes, title, body, _index, _isVisited, _isActive }, index) => {
+        return <div key={_index} className={classes(
+              "accordion__item",
+              _graphic.src && 'has-image',
+              _classes && _classes
+            )} data-index={_index}>
+          <button onClick={view.onClick.bind(view)} id={`${_id}-${index}-accordion-button`} className={classes(
+            'accordion__item-btn',
+            'js-toggle-item',
+            _isVisited && 'is-visited',
+            _isActive ? 'is-open is-selected' : 'is-closed'
+          )} aria-expanded={_isActive ? 'true' : 'false'}>
+            <div className="accordion__item-btn-inner">
+              <div className="accordion__item-icon">
+                <div className="icon"></div>
+              </div>
+              <div className="accordion__item-title">
+                <div className="accordion__item-title-inner">
+                  {html(title)}
+                </div>
+              </div>
+            </div>
+          </button>
+          <div className="accordion__item-content" role="region" aria-labelledby={`${_id}-${index}-accordion-button`}>
+            <div className="accordion__item-content-inner">
+              {body &&
+              <div className="accordion__item-body">
+                <div className="accordion__item-body-inner">
+                  {html(compile(body))}
+                </div>
+              </div>
+              }
+              {_graphic.src &&
+              <div className={classes(
+                  'accordion__item-image-container',
+                  _graphic.attribution && 'has-attribution'
+                )}>
+                <img className="accordion__item-image" src={_graphic.src} aria-label={_graphic.alt} aria-hidden={_graphic.alt ? 'true' : 'false'} />
+                {_graphic.attribution &&
+                <div className="component__attribution accordion__attribution">
+                  <div className="component__attribution-inner accordion__attribution-inner">
+                    {html(_graphic.attribution)}
+                  </div>
+                </div>
+                }
+              </div>
+              }
+            </div>
+          </div>
+        </div>
+      })}
+    </div>
+  </div>
+}
