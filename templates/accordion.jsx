83b41a689b29163022d1be0589a4eeee8d8568a5
Oliver Foster <oliver.foster@kineo.com>
2022-03-22 18:48:19 +0000

    issue/2621 Align accordion with wcag guidelines

--- templates/accordion.jsx
+++ templates/accordion.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import a11y from 'core/js/a11y';
 import { html, classes, compile, templates } from 'core/js/reactHelpers';
 
 export default function Accordion (props) {
+  const inc = ariaLevel => _.isNumber(ariaLevel) ? ariaLevel + 1 : ariaLevel;
   const {
     _id,
+    _ariaLevel,
     onClick
   } = props;
   return (
@@ -26,35 +29,39 @@ export default function Accordion (props) {
             data-index={_index}
           >
 
-            <button
-              id={`${_id}-${index}-accordion-button`}
-              className={classes([
-                'accordion-item__btn',
-                'js-toggle-item',
-                _isVisited && 'is-visited',
-                _isActive ? 'is-open is-selected' : 'is-closed'
-              ])}
-              onClick={onClick}
-              aria-expanded={_isActive.toString()}
-            >
+            <div role="heading" aria-level={a11y.ariaLevel('componentItem', inc(_ariaLevel))} >
+              <button
+                id={`${_id}-${index}-accordion-button`}
+                className={classes([
+                  'accordion-item__btn',
+                  'js-toggle-item',
+                  _isVisited && 'is-visited',
+                  _isActive ? 'is-open is-selected' : 'is-closed'
+                ])}
+                onClick={onClick}
+                aria-expanded={_isActive.toString()}
+                aria-controls={`${_id}-${index}-accordion-button-panel`}
+              >
 
-              <div className="accordion-item__btn-inner">
+                <div className="accordion-item__btn-inner">
 
-                <div className="accordion-item__icon">
-                  <div className="icon"></div>
-                </div>
-
-                <div className="accordion-item__title">
-                  <div className="accordion-item__title-inner">
-                    {html(compile(title))}
+                  <div className="accordion-item__icon">
+                    <div className="icon"></div>
                   </div>
+
+                  <div className="accordion-item__title">
+                    <div className="accordion-item__title-inner">
+                      {html(compile(title))}
+                    </div>
+                  </div>
+
                 </div>
 
-              </div>
-
-            </button>
+              </button>
+            </div>
 
             <div
+              id={`${_id}-${index}-accordion-button-panel`}
               className="accordion-item__content js-accordion-item-content"
               role="region"
               aria-labelledby={`${_id}-${index}-accordion-button`}
