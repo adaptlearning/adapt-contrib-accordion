206dd6006a8c1798c9c62da6f365fd5fc1b0416f
Guy Willis <guy.willis@kineo.com>
2023-01-16 13:36:16 +0000

    New ariaLabel variable + compiled to pass html

--- templates/accordion.jsx
+++ templates/accordion.jsx
@@ -18,77 +18,82 @@ export default function Accordion (props) {
 
       <div className="component__widget accordion__widget">
 
-        {props._items.map(({ _graphic, _imageAlignment, _classes, title, body, _index, _isVisited, _isActive, _ariaLevel }, index) =>
+        {props._items.map(({ _graphic, _imageAlignment, _classes, title, body, _index, _isVisited, _isActive, _ariaLevel }, index) => {
 
-          <div
-            className={classes([
-              'accordion-item',
-              'js-accordion-item',
-              _graphic?.src && 'has-image',
-              _graphic?.src && _imageAlignment && `align-image-${_imageAlignment}`,
-              _classes
-            ])}
-            key={_index}
-            data-index={_index}
-          >
+          const ariaLabel = `${_isVisited ? visited + '. ' : ''}${compile(title)}`;
 
-            <div role="heading" aria-level={a11y.ariaLevel({ id: _id, level: 'componentItem', override: _ariaLevel ?? itemAriaLevel })} >
-              <button
-                id={`${_id}-${index}-accordion-button`}
-                className={classes([
-                  'accordion-item__btn',
-                  'js-toggle-item',
-                  _isVisited && 'is-visited',
-                  _isActive ? 'is-open is-selected' : 'is-closed'
-                ])}
-                onClick={onClick}
-                aria-expanded={_isActive.toString()}
-                aria-controls={`${_id}-${index}-accordion-button-panel`}
-              >
-
-                <span className="accordion-item__btn-inner">
-
-                  <span className="accordion-item__icon">
-                    <span className="icon" aria-hidden="true"></span>
-                  </span>
-
-                  <span className="accordion-item__title">
-                    <span className="aria-label">{`${_isVisited ? visited + '. ' : ''}${compile(title)}`}</span>
-                    <span className="accordion-item__title-inner" aria-hidden="true" dangerouslySetInnerHTML={{ __html: compile(title) }}>
-                    </span>
-                  </span>
-
-                </span>
-
-              </button>
-            </div>
+          return (
 
             <div
-              id={`${_id}-${index}-accordion-button-panel`}
-              className="accordion-item__content js-accordion-item-content"
-              role="region"
-              aria-labelledby={`${_id}-${index}-accordion-button`}
+              className={classes([
+                'accordion-item',
+                'js-accordion-item',
+                _graphic?.src && 'has-image',
+                _graphic?.src && _imageAlignment && `align-image-${_imageAlignment}`,
+                _classes
+              ])}
+              key={_index}
+              data-index={_index}
             >
 
-              <div className="accordion-item__content-inner">
+              <div role="heading" aria-level={a11y.ariaLevel({ id: _id, level: 'componentItem', override: _ariaLevel ?? itemAriaLevel })} >
+                <button
+                  id={`${_id}-${index}-accordion-button`}
+                  className={classes([
+                    'accordion-item__btn',
+                    'js-toggle-item',
+                    _isVisited && 'is-visited',
+                    _isActive ? 'is-open is-selected' : 'is-closed'
+                  ])}
+                  onClick={onClick}
+                  aria-expanded={_isActive.toString()}
+                  aria-controls={`${_id}-${index}-accordion-button-panel`}
+                >
 
-                {body &&
-                <div className="accordion-item__body">
-                  <div className="accordion-item__body-inner" dangerouslySetInnerHTML={{ __html: compile(body) }}>
-                  </div>
-                </div>
-                }
+                  <span className="accordion-item__btn-inner">
 
-                <templates.image {..._graphic}
-                  classNamePrefixes={['component-item', 'accordion-item']}
-                  attributionClassNamePrefixes={['component', 'accordion']}
-                />
+                    <span className="accordion-item__icon">
+                      <span className="icon" aria-hidden="true"></span>
+                    </span>
 
+                    <span className="accordion-item__title">
+                      <span className="aria-label" dangerouslySetInnerHTML={{ __html: compile(ariaLabel) }}></span>
+                      <span className="accordion-item__title-inner" aria-hidden="true" dangerouslySetInnerHTML={{ __html: compile(title) }}>
+                      </span>
+                    </span>
+
+                  </span>
+
+                </button>
               </div>
-            </div>
 
-          </div>
-        )}
+              <div
+                id={`${_id}-${index}-accordion-button-panel`}
+                className="accordion-item__content js-accordion-item-content"
+                role="region"
+                aria-labelledby={`${_id}-${index}-accordion-button`}
+              >
+
+                <div className="accordion-item__content-inner">
+
+                  {body &&
+                  <div className="accordion-item__body">
+                    <div className="accordion-item__body-inner" dangerouslySetInnerHTML={{ __html: compile(body) }}>
+                    </div>
+                  </div>
+                  }
+
+                  <templates.image {..._graphic}
+                    classNamePrefixes={['component-item', 'accordion-item']}
+                    attributionClassNamePrefixes={['component', 'accordion']}
+                  />
+
+                </div>
+              </div>
+
+            </div>
+          )
+        })}
 
       </div>
 
