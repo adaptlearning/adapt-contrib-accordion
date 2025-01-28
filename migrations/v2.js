c15d411aa43506b1515b9a742efd70720b18b742
Joseph Replin <joe.replin@kineo.com>
2025-01-28 16:18:19 -0600

    v2 & v4 too early for v2.4.0 of FW
    Waiting for confirmation.

--- migrations/v2.js
+++ migrations/v2.js
@@ -1,182 +1,186 @@
-import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
+// import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
-/**
- * ? Should we be adding changes to course aria attributes? - https://github.com/adaptlearning/adapt-contrib-accordion/commit/6dca48baf740d7117ab987378ce4270c8580395b
- */
-describe('adapt-contrib-accordion - v1.0.0 > v2.0.0', async () => {
-  let accordions;
+// /**
+//  * ? Should we be adding changes to course aria attributes? - https://github.com/adaptlearning/adapt-contrib-accordion/commit/6dca48baf740d7117ab987378ce4270c8580395b
+//  */
+// describe('adapt-contrib-accordion - v1.0.0 > v2.0.0', async () => {
+//   let accordions;
 
-  whereFromPlugin('adapt-contrib-accordion - from v1', { name: 'adapt-contrib-accordion', version: '<=2.0.0' });
+//   whereFromPlugin('adapt-contrib-accordion - from v1', { name: 'adapt-contrib-accordion', version: '<=2.0.0' });
 
-  whereContent('adapt-contrib-accordion - where accordion', async content => {
-    accordions = content.filter(({ _component }) => _component === 'accordion');
-    if (accordions) return true;
-  });
+//   whereContent('adapt-contrib-accordion - where accordion', async content => {
+//     accordions = content.filter(({ _component }) => _component === 'accordion');
+//     if (accordions) return true;
+//   });
 
-  /**
-   * * Add JSON field to component and set attribute.
-   */
-  mutateContent('adapt-contrib-accordion - add accordion._supportedLayout', async () => {
-    accordions.forEach(accordion => {
-      accordion._supportedLayout = 'half-width';
-    });
-    return true;
-  });
+//   /**
+//    * * Add JSON field to component and set attribute.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion._supportedLayout', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._supportedLayout = 'half-width';
+//     });
+//     return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check accordion._supportedLayout atrribute', async () => {
-    const isValid = accordions.every(({ _supportedLayout }) => _supportedLayout !== undefined);
-    if (!isValid) throw new Error('adapt-contrib-accordion - _supportedLayout not added to every instance of accordion');
-    return true;
-  });
-  mutateContent('adapt-contrib-accordion - add accordion.instruction', async () => {
-    accordions.forEach(accordion => {
-      accordion.instruction = '';
-    });
-    return true;
-  });
+//   checkContent('adapt-contrib-accordion - check accordion._supportedLayout atrribute', async () => {
+//     const isValid = accordions.every(({ _supportedLayout }) => _supportedLayout !== undefined);
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _supportedLayout not added to every instance of accordion');
+//     return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check accordion.instruction atrribute', async () => {
-    const isValid = accordions.every(({ instruction }) => instruction !== undefined);
-    if (!isValid) throw new Error('adapt-contrib-accordion - instruction not added to every instance of accordion');
-    return true;
-  });
+//   /**
+//    * * Add JSON field to component and set blank.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion.instruction', async () => {
+//     accordions.forEach(accordion => {
+//       accordion.instruction = '';
+//     });
+//     return true;
+//   });
 
-  /**
-   * * Add field to each item in a JSON array and set blank.
-   */
-  mutateContent('adapt-contrib-accordion - add accordion._items._graphic.alt attribute', async () => {
-    accordions.forEach(accordion => {
-      accordion._items.forEach(item => {
-        item._graphic.alt = '';
-      });
-    });
-    return true;
-  });
+//   checkContent('adapt-contrib-accordion - check accordion.instruction atrribute', async () => {
+//     const isValid = accordions.every(({ instruction }) => instruction !== undefined);
+//     if (!isValid) throw new Error('adapt-contrib-accordion - instruction not added to every instance of accordion');
+//     return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check add accordion._items._graphic.alt atrribute', async () => {
-    const isValid = accordions.every(accordion =>
-      accordion._items.every(item =>
-        item._graphic && item._graphic.alt !== undefined
-      )
-    );
-    if (!isValid) throw new Error('adapt-contrib-accordion - _graphic.alt not added to every instance of accordion._items');
-    return true;
-  });
+//   /**
+//    * * Add field to each item in a JSON array and set blank.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion._items._graphic.alt attribute', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._items.forEach(item => {
+//         item._graphic.alt = '';
+//       });
+//     });
+//     return true;
+//   });
 
-  /**
-   * * Add field to each item in a JSON array and set blank.
-   */
-  mutateContent('adapt-contrib-accordion - add accordion._items._graphic.src attribute', async () => {
-    accordions.forEach(accordion => {
-      accordion._items.forEach(item => {
-        item._graphic.src = '';
-      });
-    });
-    return true;
-  });
+//   checkContent('adapt-contrib-accordion - check add accordion._items._graphic.alt atrribute', async () => {
+//     const isValid = accordions.every(accordion =>
+//       accordion._items.every(item =>
+//         item._graphic && item._graphic.alt !== undefined
+//       )
+//     );
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _graphic.alt not added to every instance of accordion._items');
+//     return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check add accordion._items._graphic.src atrribute', async () => {
-    const isValid = accordions.every(accordion =>
-      accordion._items.every(item =>
-        item._graphic && item._graphic.src !== undefined
-      )
-    );
-    if (!isValid) throw new Error('adapt-contrib-accordion - _graphic.src not added to every instance of accordion._items');
-    return true;
-  });
+//   /**
+//    * * Add field to each item in a JSON array and set blank.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion._items._graphic.src attribute', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._items.forEach(item => {
+//         item._graphic.src = '';
+//       });
+//     });
+//     return true;
+//   });
 
-  updatePlugin('adapt-contrib-accordion - update to v2.0.0', { name: 'adapt-contrib-accordion', version: '2.0.0', framework: '2.0.0' });
-});
+//   checkContent('adapt-contrib-accordion - check add accordion._items._graphic.src atrribute', async () => {
+//     const isValid = accordions.every(accordion =>
+//       accordion._items.every(item =>
+//         item._graphic && item._graphic.src !== undefined
+//       )
+//     );
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _graphic.src not added to every instance of accordion._items');
+//     return true;
+//   });
 
-describe('adapt-contrib-accordion - v2.0.0 > v2.0.4', async () => {
-  let accordions;
+//   updatePlugin('adapt-contrib-accordion - update to v2.0.0', { name: 'adapt-contrib-accordion', version: '2.0.0', framework: '2.0.0' });
+// });
 
-  whereFromPlugin('adapt-contrib-accordion - from v2.0.0', { name: 'adapt-contrib-accordion', version: '<=2.0.4' });
+// describe('adapt-contrib-accordion - v2.0.0 > v2.0.4', async () => {
+//   let accordions;
 
-  whereContent('adapt-contrib-accordion - where accordion', async content => {
-    accordions = content.filter(({ _component }) => _component === 'accordion');
-    if (accordions) return true;
-  });
+//   whereFromPlugin('adapt-contrib-accordion - from v2.0.0', { name: 'adapt-contrib-accordion', version: '<=2.0.4' });
 
-  /**
-  * * Add JSON field to component and set blank.
-  */
-  mutateContent('adapt-contrib-accordion - add accordion._classes', async () => {
-    accordions.forEach(accordion => {
-      accordion._classes = '';
-    });
-    return true;
-  });
+//   whereContent('adapt-contrib-accordion - where accordion', async content => {
+//     accordions = content.filter(({ _component }) => _component === 'accordion');
+//     if (accordions) return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check accordion._classes atrribute', async () => {
-    const isValid = accordions.every(({ _classes }) => _classes !== undefined);
-    if (!isValid) throw new Error('adapt-contrib-accordion - _classes not added to every instance of accordion');
-    return true;
-  });
+//   /**
+//   * * Add JSON field to component and set blank.
+//   */
+//   mutateContent('adapt-contrib-accordion - add accordion._classes', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._classes = '';
+//     });
+//     return true;
+//   });
 
-  updatePlugin('adapt-contrib-accordion - update to v2.0.4', { name: 'adapt-contrib-accordion', version: '2.0.4', framework: '>=2' });
-});
+//   checkContent('adapt-contrib-accordion - check accordion._classes atrribute', async () => {
+//     const isValid = accordions.every(({ _classes }) => _classes !== undefined);
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _classes not added to every instance of accordion');
+//     return true;
+//   });
 
-describe('adapt-contrib-accordion - v2.0.4 > v2.0.5', async () => {
-  let accordions;
+//   updatePlugin('adapt-contrib-accordion - update to v2.0.4', { name: 'adapt-contrib-accordion', version: '2.0.4', framework: '>=2' });
+// });
 
-  whereFromPlugin('adapt-contrib-accordion - from v2.0.4', { name: 'adapt-contrib-accordion', version: '<=2.0.5' });
+// describe('adapt-contrib-accordion - v2.0.4 > v2.0.5', async () => {
+//   let accordions;
 
-  whereContent('adapt-contrib-accordion - where accordion', async content => {
-    accordions = content.filter(({ _component }) => _component === 'accordion');
-    if (accordions) return true;
-  });
+//   whereFromPlugin('adapt-contrib-accordion - from v2.0.4', { name: 'adapt-contrib-accordion', version: '<=2.0.5' });
 
-  /**
-   * * Add JSON field to component and set attribute.
-   */
-  mutateContent('adapt-contrib-accordion - add accordion._shouldCollapseItems', async () => {
-    accordions.forEach(accordion => {
-      accordion._shouldCollapseItems = false;
-    });
-    return true;
-  });
+//   whereContent('adapt-contrib-accordion - where accordion', async content => {
+//     accordions = content.filter(({ _component }) => _component === 'accordion');
+//     if (accordions) return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check accordion._shouldCollapseItems atrribute', async () => {
-    const isValid = accordions.every(({ _classes }) => _classes !== undefined);
-    if (!isValid) throw new Error('adapt-contrib-accordion - _shouldCollapseItems not added to every instance of accordion');
-    return true;
-  });
+//   /**
+//    * * Add JSON field to component and set attribute.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion._shouldCollapseItems', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._shouldCollapseItems = false;
+//     });
+//     return true;
+//   });
 
-  updatePlugin('adapt-contrib-accordion - update to v2.0.5', { name: 'adapt-contrib-accordion', version: '2.0.5', framework: '>=2' });
-});
+//   checkContent('adapt-contrib-accordion - check accordion._shouldCollapseItems atrribute', async () => {
+//     const isValid = accordions.every(({ _classes }) => _classes !== undefined);
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _shouldCollapseItems not added to every instance of accordion');
+//     return true;
+//   });
 
-describe('adapt-contrib-accordion - v2.0.5 > v2.1.0', async () => {
-  let accordions;
+//   updatePlugin('adapt-contrib-accordion - update to v2.0.5', { name: 'adapt-contrib-accordion', version: '2.0.5', framework: '>=2' });
+// });
 
-  whereFromPlugin('adapt-contrib-accordion - from v2.1.0', { name: 'adapt-contrib-accordion', version: '<=2.1.0' });
+// describe('adapt-contrib-accordion - v2.0.5 > v2.1.0', async () => {
+//   let accordions;
 
-  whereContent('adapt-contrib-accordion - where accordion', async content => {
-    accordions = content.filter(({ _component }) => _component === 'accordion');
-    if (accordions) return true;
-  });
+//   whereFromPlugin('adapt-contrib-accordion - from v2.1.0', { name: 'adapt-contrib-accordion', version: '<=2.1.0' });
 
-  /**
-   * * Add field to each item in a JSON array and set blank.
-   */
-  mutateContent('adapt-contrib-accordion - add accordion._items._graphic.attribution', async () => {
-    accordions.forEach(accordion => {
-      accordion._items.forEach(item => {
-        item._graphic.attribution = '';
-      });
-    });
-    return true;
-  });
+//   whereContent('adapt-contrib-accordion - where accordion', async content => {
+//     accordions = content.filter(({ _component }) => _component === 'accordion');
+//     if (accordions) return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check accordion._items._graphic.attribution atrribute', async () => {
-    const isValid = accordions.every(accordion =>
-      accordion._items.every(item =>
-        item._graphic && item._graphic.attribution !== undefined
-      )
-    );
-    if (!isValid) throw new Error('adapt-contrib-accordion - _graphic.attribution not added to every instance of accordion._items');
-    return true;
-  });
+//   /**
+//    * * Add field to each item in a JSON array and set blank.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion._items._graphic.attribution', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._items.forEach(item => {
+//         item._graphic.attribution = '';
+//       });
+//     });
+//     return true;
+//   });
 
-  updatePlugin('adapt-contrib-accordion - update to v2.1.0', { name: 'adapt-contrib-accordion', version: '2.1.0', framework: '>=2' });
-});
+//   checkContent('adapt-contrib-accordion - check accordion._items._graphic.attribution atrribute', async () => {
+//     const isValid = accordions.every(accordion =>
+//       accordion._items.every(item =>
+//         item._graphic && item._graphic.attribution !== undefined
+//       )
+//     );
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _graphic.attribution not added to every instance of accordion._items');
+//     return true;
+//   });
+
+//   updatePlugin('adapt-contrib-accordion - update to v2.1.0', { name: 'adapt-contrib-accordion', version: '2.1.0', framework: '>=2' });
+// });
--- migrations/v4.js
+++ migrations/v4.js
@@ -1,30 +1,30 @@
-import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
+// import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
-describe('adapt-contrib-accordion - v2.1.0 > v4.0.0', async () => {
-  let accordions;
+// describe('adapt-contrib-accordion - v2.1.0 > v4.0.0', async () => {
+//   let accordions;
 
-  whereFromPlugin('adapt-contrib-accordion - from v2.1.0', { name: 'adapt-contrib-accordion', version: '<=4.0.0' });
+//   whereFromPlugin('adapt-contrib-accordion - from v2.1.0', { name: 'adapt-contrib-accordion', version: '<=4.0.0' });
 
-  whereContent('adapt-contrib-accordion - where accordion', async content => {
-    accordions = content.filter(({ _component }) => _component === 'accordion');
-    if (accordions) return true;
-  });
+//   whereContent('adapt-contrib-accordion - where accordion', async content => {
+//     accordions = content.filter(({ _component }) => _component === 'accordion');
+//     if (accordions) return true;
+//   });
 
-  /**
-   * * Add JSON field to component and set attribute.
-   */
-  mutateContent('adapt-contrib-accordion - add accordion._setCompletionOn', async () => {
-    accordions.forEach(accordion => {
-      accordion._setCompletionOn = 'allItems';
-    });
-    return true;
-  });
+//   /**
+//    * * Add JSON field to component and set attribute.
+//    */
+//   mutateContent('adapt-contrib-accordion - add accordion._setCompletionOn', async () => {
+//     accordions.forEach(accordion => {
+//       accordion._setCompletionOn = 'allItems';
+//     });
+//     return true;
+//   });
 
-  checkContent('adapt-contrib-accordion - check accordion._setCompletionOn atrribute', async () => {
-    const isValid = accordions.every(({ _setCompletionOn }) => _setCompletionOn !== undefined);
-    if (!isValid) throw new Error('adapt-contrib-accordion - _setCompletionOn not added to every instance of accordion');
-    return true;
-  });
+//   checkContent('adapt-contrib-accordion - check accordion._setCompletionOn atrribute', async () => {
+//     const isValid = accordions.every(({ _setCompletionOn }) => _setCompletionOn !== undefined);
+//     if (!isValid) throw new Error('adapt-contrib-accordion - _setCompletionOn not added to every instance of accordion');
+//     return true;
+//   });
 
-  updatePlugin('adapt-contrib-accordion - update to v2.0.0', { name: 'adapt-contrib-accordion', version: '2.0.0', framework: '2.0.0' });
-});
+//   updatePlugin('adapt-contrib-accordion - update to v2.0.0', { name: 'adapt-contrib-accordion', version: '2.0.0', framework: '2.0.0' });
+// });
