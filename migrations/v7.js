d2923bea4dee022757a7a65a6f21638c6e814ead
Joseph Replin <joe.replin@kineo.com>
2025-01-30 13:34:58 -0600

    Update migrations/v7.js
    Co-authored-by: joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>

--- migrations/v7.js
+++ migrations/v7.js
@@ -63,7 +63,7 @@ describe('adapt-contrib-accordion - v7.3.0 > v7.4.0', async () => {
     return true;
   });
 
-  updatePlugin('adapt-contrib-accordion - update to v7.4.0', { name: 'adapt-contrib-accordion', version: '7.4.0', framework: '>=5' });
+  updatePlugin('adapt-contrib-accordion - update to v7.4.0', { name: 'adapt-contrib-accordion', version: '7.4.0', framework: '>5.20.1' });
 });
 
 describe('adapt-contrib-accordion - v7.4.0 > v7.7.0', async () => {
